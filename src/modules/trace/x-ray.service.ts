1854749e57b97322b4b4a5ffd40c601fce7a2f88
Rick Waterman <rwaterman+noreply@github.com>
2020-05-25 11:19:35 -0700

    chore: bump packages, fix .gitignore for Jetbrains

--- src/modules/trace/x-ray.service.ts
+++ src/modules/trace/x-ray.service.ts
@@ -1,10 +1,7 @@
-import { S3 } from 'aws-sdk';
-import * as AWSXRay from 'aws-xray-sdk';
 import { injectable } from 'inversify';
 import { ITrace } from '../log/models/interfaces';
 import * as AWS_SDK from 'aws-sdk';
 import * as AWS_XRAY from 'aws-xray-sdk';
-import { PatchedAWS } from 'aws-xray-sdk-core/lib/patchers/aws_p';
 
 export interface IService {
   name: string;
@@ -18,7 +15,7 @@ interface IXRayService extends IService {
 @injectable()
 export class XRayService implements ITrace {
   private services: IXRayService[] = [];
-  private xray: PatchedAWS;
+  private xray: any;
 
   constructor() {
     this.xray = AWS_XRAY.captureAWS(AWS_SDK);
